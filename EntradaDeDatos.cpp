e96653f545c72afb9b0b6b886e2f21667f80f6c6
jhotacode <125765909+jhotacode@users.noreply.github.com>
2024-06-06 09:24:09 -0500

    Aprendiendo a ingresar los datos en las variables

---
+++ EntradaDeDatos.cpp
@@ -0,0 +1,20 @@
+//Lectura o entrada de datos
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numero; 
+	cout<<"Digite un numero: "; //Esto son comentarios
+	cin>>numero;/*Este es
+	un comentario en 
+	varias lineas
+		*/
+	
+	cout<<"\nEl numero es: "<<numero;
+	
+	
+	return 0;
+}
