bcea971a9cc5cdac4be67816bf931be36a764fe9
jhotacode <125765909+jhotacode@users.noreply.github.com>
2024-06-05 23:14:00 -0500

    Conociendo los tipos de datos en C++

---
+++ TiposDatos.cpp
@@ -0,0 +1,24 @@
+//Tipos de datos básicos en C++
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int numero = 15; //Una variable es un espacio en memoria
+	cout<<numero<<endl;
+	
+	float numero2 = 3.1416;
+	cout<<numero2<<endl;
+	
+	double numero3 = 1.23456789;
+	cout<<numero3<<endl;
+	
+	char letra = 'a';
+	cout<<letra<<endl;
+	
+	
+	return 0;
+}
