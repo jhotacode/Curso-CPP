ba231fe2369eff0e71fe04c7fb80f51e92021f63
jhotacode <125765909+jhotacode@users.noreply.github.com>
2024-06-06 17:15:07 -0500

    Ejercicio en C++ para ingresar y mostrar los datos

---
+++ EntradaDeDatos2.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	
+	int edad;
+	char sexo[10];
+	float altura;
+	
+	cout<<"Ingrese su edad: ";
+	cin>>edad;
+	
+	cout<<"Ingrese su altura: ";
+	cin>>altura;
+	
+	cout<<"Ingrese su sexo";
+	cin>>sexo;
+	
+	cout<<"DATOS INGRESADOS"<<endl;
+	cout<<"Edad: "<<edad<<"\nAltura: "<<altura<<"\nSexo: "<<sexo;
+	
+	return 0;
+}
