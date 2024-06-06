aeda11f6bac89c7d2b60f5ad95b3f39abbd40b44
jhotacode <125765909+jhotacode@users.noreply.github.com>
2024-06-06 09:39:54 -0500

    Primer ejercicio aritmetico de C++

---
+++ EjercicioAritmetico.cpp
@@ -0,0 +1,22 @@
+//Crear una calculadora sencilla
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int numero1,numero2; 
+	cout<<"Ingrese el primer operador: ";
+	cin>>numero1;
+	
+	cout<<"Ingrese elsegundo operador: ";
+	cin>>numero2;
+	
+	cout<<"Suma: "<<numero1+numero2<<endl;
+	cout<<"Resta: "<<numero1-numero2<<endl;
+	cout<<"Multiplicacion: "<<numero1*numero2<<endl;
+	cout<<"Division: "<<numero1/numero2<<endl;
+	
+	
+	return 0;
+}
