b05435b00a045ef1a3fd08b6ad44893a97bbc841
jhotacode <125765909+jhotacode@users.noreply.github.com>
2024-06-06 21:01:33 -0500

    Aprendiendo sobre los condicionales en C++

---
+++ Condicionales.cpp
@@ -0,0 +1,49 @@
+/*
+En este bloque vamos a aprender sobre los bloques condicionales en C++.
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	//Sentencia IF
+	/*
+	int numero1,numero2 = 1000;
+	cout<<"Ingrese un numero: ";
+	cin>>numero1;
+	
+	if(numero1==numero2)
+	{
+		cout<<"Adivinaste el numero";
+	}
+	else
+	{
+		cout<<"No lo adivinaste";
+	}
+	*/
+	//Sentencia Switch
+	/*
+	int eleccion;
+	cout<<"Ingresa tu opción favorita:\n1. Jugo de Maracuya\n2. Jugo de Mora\n3. Jugo de Naranja"<<endl<<">>";
+	cin>>eleccion;
+	switch(eleccion)
+	{
+		case 1:
+			cout<<"Jugo de los dioses";
+			break;
+		case 2:
+			cout<<"Jugo oficial del olimpo";
+			break;
+		case 3: 
+			cout<<"Jugo de eterna sabiduría";
+			break;
+		default:
+			cout<<"Estas loco o que mi perro?";
+	}
+	*/
+	
+	
+	return 0;
+}
+
